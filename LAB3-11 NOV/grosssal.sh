5481d24c713da221e88bec187d5872b4733d8f55
SAI-RAMAKRISHNA <71307060+SAI-RAMAKRISHNA@users.noreply.github.com>
2021-11-11 12:39:08 +0530

    Delete grosssal.sh

--- LAB3-11 NOV/grosssal.sh
+++
@@ -1,7 +0,0 @@
-echo " enter the basic salary"
-read basic
-DA=`echo "$basic * 0.1"|bc`
-HRA=`echo "$basic * 0.2"|bc`
-grosssalary=`echo "$basic + $DA + $HRA"|bc`
-echo " Gross salary = $grosssalary"
-
